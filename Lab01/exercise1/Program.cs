8a9e3de093e3c0e50ef8c58593175411cf1c93f4
Jakub <jakubowskijjal1@gmail.com>
2024-01-22 18:12:28 +0100

    Initial commit

---
+++ Lab01/exercise1/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("input a number: ");
+int userInput = int.Parse(Console.ReadLine());
+Console.WriteLine(userInput.GetType());
+
+if(userInput == 0 )
+{
+    Console.WriteLine("Your number is 0");
+}
+else if(userInput > 0)
+{
+    if(userInput %2 == 0)
+    {
+        Console.WriteLine("Your number is above 0 and even");
+    }
+    else
+    {
+        Console.WriteLine("Your number is aboce 0 and odd");
+    }
+}
+else
+{
+    if (userInput % 2 == 0)
+    {
+        Console.WriteLine("Your number is less than 0 and even");
+    }
+    else
+    {
+        Console.WriteLine("Your number is less than 0 and odd");
+    }
+        
+}
---
+++ Lab01/exercise3/Program.cs
@@ -0,0 +1,10 @@
+﻿String[] url = System.IO.File.ReadAllLines("C:\\Users\\Admin\\IdeaProjects\\C# - Studies\\Lab01\\exercise3\\iris.csv");
+foreach (string item in url)
+{
+    String[] line = item.Split(',');
+    foreach(string substrings in line)
+    {
+        Console.Write(substrings+" ");
+    }
+    Console.WriteLine();
+}
